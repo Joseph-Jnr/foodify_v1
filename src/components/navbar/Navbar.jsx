dbcbec1d04c9d106aa57a0a2673c69f460b60078
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-04-17 10:31:59 +0100

    temp: trying out another mobile nav variant

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { MdShoppingBasket, MdLightMode } from 'react-icons/md'
 import { CiDark, CiLogin } from 'react-icons/ci'
-import { RiArrowDownSLine } from 'react-icons/ri'
+import { RiArrowDownSLine, RiServiceLine } from 'react-icons/ri'
+import { BiHomeAlt, BiFoodMenu } from 'react-icons/bi'
 import { Link, NavLink, useLocation } from 'react-router-dom'
 import './Navbar.css'
 import Button from '../UI/button/Button'
@@ -69,75 +70,121 @@ const Navbar = () => {
   }, [])
 
   return (
-    <nav className={`navbarComponent ${isSticky ? 'sticky' : ''}`}>
-      <div className='container flex items-center justify-between'>
-        {window.innerWidth <= 765 ? (
-          <div
-            className='navbar-brand flex items-center'
-            onClick={toggleMobileNav}
-          >
-            Foodify{' '}
-            <span className={`ml-2 ${showMobileNav ? 'rotate' : ''}`}>
-              <RiArrowDownSLine size={20} />
-            </span>
-          </div>
-        ) : (
-          <Link to='/' className='navbar-brand flex items-center'>
-            Foodify
-          </Link>
-        )}
+    <>
+      <nav className={`navbarComponent ${isSticky ? 'sticky' : ''}`}>
+        <div className='container flex items-center justify-between'>
+          {window.innerWidth <= 765 ? (
+            <div
+              className='navbar-brand flex items-center'
+              onClick={toggleMobileNav}
+            >
+              Foodify{' '}
+              <span className={`ml-2 ${showMobileNav ? 'rotate' : ''}`}>
+                <RiArrowDownSLine size={20} />
+              </span>
+            </div>
+          ) : (
+            <Link to='/' className='navbar-brand flex items-center'>
+              Foodify
+            </Link>
+          )}
 
-        <div className={showMobileNav ? 'nav-links-mobile' : 'nav-links'}>
+          <div className={showMobileNav ? 'nav-links-mobile' : 'nav-links'}>
+            <ul>
+              <li>
+                <NavLink exact='true' to='/' activeclassname='active'>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/menu' activeclassname='active'>
+                  Menu
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/services' activeclassname='active'>
+                  Services
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/cart' className='flex items-center'>
+                  Cart
+                  <span className='counter-wrap ml-1'>
+                    <span className='cart-counter'>2</span>
+                    <span className='cart-icon'>
+                      <MdShoppingBasket />
+                    </span>
+                  </span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className='right-actions flex items-center'>
+            <div
+              className={`theme-switcher ${isHomePage ? 'white' : ''}`}
+              onClick={handleThemeToggle}
+            >
+              {/* Dark theme switcher */}
+              <span>{dark ? <MdLightMode /> : <CiDark />}</span>
+            </div>
+            <div className='login-btn'>
+              <Link to='/login'>
+                <Button
+                  icon={<CiLogin />}
+                  btnClass={'main-btn flex items-center'}
+                  iconClass={'mr-2'}
+                  text={'Login'}
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      {/* {window.innerWidth <= 765 ? (
+        <div className='mobile-nav'>
           <ul>
             <li>
               <NavLink exact='true' to='/' activeclassname='active'>
+                <span>
+                  <BiHomeAlt />
+                </span>
                 Home
               </NavLink>
             </li>
             <li>
               <NavLink to='/menu' activeclassname='active'>
+                <span>
+                  <BiFoodMenu />
+                </span>
                 Menu
               </NavLink>
             </li>
             <li>
               <NavLink to='/services' activeclassname='active'>
+                <span>
+                  <RiServiceLine />
+                </span>
                 Services
               </NavLink>
             </li>
             <li>
               <NavLink to='/cart' className='flex items-center'>
-                Cart
                 <span className='counter-wrap ml-1'>
                   <span className='cart-counter'>2</span>
                   <span className='cart-icon'>
                     <MdShoppingBasket />
                   </span>
                 </span>
+                Cart
               </NavLink>
             </li>
           </ul>
         </div>
-        <div className='right-actions flex items-center'>
-          <div
-            className={`theme-switcher ${isHomePage ? 'white' : ''}`}
-            onClick={handleThemeToggle}
-          >
-            {/* Dark theme switcher */}
-            <span>{dark ? <MdLightMode /> : <CiDark />}</span>
-          </div>
-          <div className='login-btn'>
-            <Link to='/login'>
-              <Button
-                icon={<CiLogin />}
-                btnClass={'main-btn flex items-center'}
-                iconClass={'mr-2'}
-                text={'Login'}
-              />
-            </Link>
-          </div>
-        </div>
-      </div>
-    </nav>
+      ) : (
+        <></>
+      )} */}
+    </>
   )
 }
 
