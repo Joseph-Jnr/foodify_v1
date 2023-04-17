eb7ee8bbe14d5e7f7ce5471dda95ae6a666a63aa
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-04-17 10:26:36 +0100

    update: redesigned food cards

--- src/components/foodCards/FoodCardItem.jsx
+++ src/components/foodCards/FoodCardItem.jsx
@@ -2,27 +2,36 @@ import React from 'react'
 import './FoodCardItem.css'
 import { MdShoppingBasket } from 'react-icons/md'
 import { AiOutlineTags } from 'react-icons/ai'
+import { TbTruckDelivery } from 'react-icons/tb'
 import Button from '../UI/button/Button'
 
-const FoodCardItem = ({ image, title, category, price }) => {
+const FoodCardItem = ({ image, title, category, delivery, price }) => {
   return (
-    <div className='single-food-card col-md-4'>
+    <div className='single-food-card'>
       <div className='img-area'>
         <img src={image} alt='food image' />
       </div>
       <div className='content'>
-        <h1>{title}</h1>
-        <p className='flex items-center'>
-          <span className='mr-1'>
-            <AiOutlineTags />
-          </span>{' '}
-          {category}
-        </p>
+        <div className='description'>
+          <h1>{title}</h1>
+          <p className='flex items-center'>
+            <span className='mr-1'>
+              <AiOutlineTags />
+            </span>{' '}
+            {category}
+          </p>
+          <p className='flex items-center'>
+            <span className='mr-1'>
+              <TbTruckDelivery />
+            </span>{' '}
+            {delivery}
+          </p>
+        </div>
         <div className='price'>
           <span className='amount'>{price.toLocaleString()}</span>{' '}
           <span className='currency'>FCFA</span>
         </div>
-        <div className='btns'>
+        {/* <div className='btns'>
           <Button
             icon={<MdShoppingBasket />}
             btnClass={'black-btn hasIcon'}
@@ -30,7 +39,7 @@ const FoodCardItem = ({ image, title, category, price }) => {
             href={''}
             text={'Add to cart'}
           />
-        </div>
+        </div> */}
       </div>
     </div>
   )
