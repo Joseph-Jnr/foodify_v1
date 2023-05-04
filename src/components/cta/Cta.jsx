e0373d7cfb46b51820222532fc8ed686e72fe695
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-05-04 12:04:47 +0100

    whatsapp contact redirect

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -4,6 +4,12 @@ import qrCode from '../../assets/qr.png'
 import Button from '../UI/button/Button'
 
 const Cta = () => {
+  const handleWhatsAppButtonClick = () => {
+    const phoneNumber = '22990573130'
+    const message = encodeURIComponent('Hi Foodify, I want to...')
+    window.location.href = `https://wa.me/${phoneNumber}/?text=${message}`
+  }
+
   return (
     <div className='section-padding cta-section'>
       <div className='container'>
@@ -19,7 +25,11 @@ const Cta = () => {
               <div className='qr-scan flex justify-center'>
                 <img src={qrCode} alt='' />
               </div>
-              <Button btnClass={'black-btn'} text={'Contact us'} />
+              <Button
+                btnClass={'black-btn'}
+                onClick={handleWhatsAppButtonClick}
+                text={'Contact us'}
+              />
             </div>
           </div>
         </div>
